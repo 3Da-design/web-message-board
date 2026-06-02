2798fb94cf243ca614164b8d79e918d009dc53cd
Y.S <184586909+Yuto-design@users.noreply.github.com>
2026-06-02 15:14:00 +0900

    inital

---
+++ public/index.php
@@ -0,0 +1,78 @@
+<?php
+
+session_start();
+require_once(__DIR__ . '/../src/db_connect.php');
+
+if (isset($_POST['action_type']) && $_POST['action_type']) {
+  if ($_POST['action_type'] === 'insert') {
+    require(__DIR__ . '/../src/insert_message.php');
+  }
+}
+
+require(__DIR__ . '/../src/session_values.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>ひとこと掲示板</title>
+    <link rel="stylesheet" href="./assets/main.css">
+  </head>
+
+  <body>
+    <div class="page-cover">
+      <p class="page-title">ひとこと掲示板</p>
+
+      <div class="form-cover">
+        <form action="/" method="post">
+          <!-- 投稿者ニックネームの入力フォーム -->
+          <div class="form-input-title">投稿者ニックネーム</div>
+          <input
+            type-"text"
+            name="author_name"
+            maxlength="40"
+            value="<?php echo htmlspecialchars($messages['input_pre_author_name'], ENT_QUOTES); ?>"
+            class=input-author-name
+          />
+          <?php if ($messages['input_error_author_name'] !== '') { ?>
+            <div class="form-input-error">
+              <?= $messages['input_error_author_name']; ?>
+            </div>
+          <?php } ?>
+
+          <!-- 投稿内容の入力フォーム -->
+          <div class="form-input-title">
+            投稿内容
+            <small>(必須)</small>
+          </div>
+          <textarea
+            name="message"
+            class="input-message"
+          >
+            <?php echo htmlspecialchars($messages['input_pre_message'], ENT_QUOTES); ?>
+          </textarea>
+          <?php if ($messages['input_error_message'] !== '') { ?>
+            <div class="form-input-error">
+              <?= $messages['input_error_message']; ?>
+            </div>
+          <?php } ?>
+
+          <!-- 投稿ボタン -->
+          <input type="hidden" name="action_type" value="insert" />
+          <button type="submit" class="input-submit-button">投稿する</button>
+        </form>
+      </div>
+
+      <hr class="page-divider"/>
+
+      <div class="message-list-cover">
+        <!-- メッセージ一覧の内容 -->
+      </div>
+    </div>
+  </body>
+</html>
---
+++ src/insert_message.php
@@ -0,0 +1,69 @@
+<?php
+
+function mbTrim($pString) {
+  return preg_replace('/^[\p{C}\p{Z}]+|[\p{C}\p{Z}]+$/u', '', $pString);
+}
+
+// 入力値を確認する（投稿者）
+$is_valid_author_name = true;
+$input_author_name = '';
+if (isset($_POST['author_name'])) {
+  $input_author_name = mbTrim(str_replace("\r\n", "\n", $_POST['author_name']));
+  $_SESSION['input_pre_author_name'] = $_POST['author_name'];
+} else {
+  $is_valid_author_name = false;
+}
+
+if ($is_valid_author_name && mb_strlen($input_author_name) > 30) {
+  $is_valid_author_name = false;
+  $_SESSION['input_error_author_name'] = '投稿者ニックネームは30文字以内で入力してください。(現在 ' . mb_strlen($input_author_name) . ' 文字)';
+}
+
+// 入力値を確認する（投稿内容）
+$is_valid_message = true;
+$input_message = '';
+if (isset($_POST['message'])) {
+  $input_message = mbTrim(str_replace("\r\n", "\n", $_POST['message']));
+  $_SESSION['input_pre_message'] = $_POST['message'];
+} else {
+  $is_valid_message = false;
+}
+
+if ($is_valid_message && $input_message === '') {
+  $is_valid_message = false;
+  $_SESSION['input_error_message'] = '投稿内容は必須です。';
+}
+
+if ($is_valid_message && mb_strlen($input_message) > 1000) {
+  $is_valid_message = false;
+  $_SESSION['input_error_message'] = '投稿内容は1000文字以下で入力してください。(現在 ' . mb_strlen($input_message) . ' 文字)';
+}
+
+// 投稿をデータベースへ保存する処理
+if ($is_valid_author_name && $is_valid_message) {
+  if ($input_author_name === '') {
+    $input_author_name = '匿名さん';
+  }
+
+  // INSERT クエリを作成する
+  $query = 'INSERT INTO posts (author_name, message) VALUES (:author_name, :message)';
+
+  // SQL 実行の準備
+  $stmt = $dbh->prepare($query);
+
+  // プレースホルダに値をセット
+  $stmt->bindValue(':author_name', $input_author_name, PDO::PARAM_STR);
+  $stmt->bindValue(':message', $input_message, PDO::PARAM_STR);
+
+  // クエリを実行
+  $stmt->execute();
+  $_SESSION['input_error_author_name'] = '';
+  $_SESSION['input_error_message'] = '';
+  $_SESSION['input_pre_author_name'] = '';
+  $_SESSION['input_pre_message'] = '';
+}
+
+header('Location: /');
+exit();
+
+?>
